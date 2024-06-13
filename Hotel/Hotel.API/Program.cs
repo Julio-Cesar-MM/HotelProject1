cfedc8d79c7a76c4acd4244adc6ccd65e06736fb
juliocmm0321@gmail.com <juliocmm0321@gmail.com>
2024-06-13 16:31:29 -0700

    Add project files.

---
+++ Hotel/Hotel.API/Program.cs
@@ -0,0 +1,49 @@
+
+//using Hotel.Application.Contracts;
+//using Hotel.Application.Services;
+//using Hotel.Infraestructure.Interfaces;
+//using Hotel.Infraestructure.Repositories;
+using Hotel.Infraestructure.Context;
+using Hotel.Ioc.Dependencies;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Agregar dependencias del contexto //
+builder.Services.AddDbContext<HotelContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("HotelContext")));
+
+// ****Dependencias de los repositorios**********
+
+//builder.Services.AddTransient<IClienteRepository, ClienteRepository>();
+//builder.Services.AddTransient<IRecepcionRepository, RecepcionRepository>();
+
+builder.Services.AddClienteDependency();
+builder.Services.AddRecepcionDependency();
+
+
+
+
+
+// Dependencias de los app services //
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
